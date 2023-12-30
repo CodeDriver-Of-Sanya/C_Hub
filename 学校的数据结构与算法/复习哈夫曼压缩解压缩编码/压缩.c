e27708b0b7de197faa535026ce1646bae74f4e21
WinternetQilin <96943787+WinternetQilin@users.noreply.github.com>
2023-12-30 13:46:49 +0800

    开始复习数据结构与算法了

---
+++ 学校的数据结构与算法/复习哈夫曼压缩解压缩编码/压缩.c
@@ -0,0 +1,19 @@
+#include"Res.h"
+
+void analysisFile(char* secFile, struct Zidian* pzd);
+
+int main()
+{
+	//待压缩文件名为 1.txt
+
+	//1.分析待压缩文件，得到字典
+	struct Zidian zd = { 0 };
+
+
+	return 0;
+}
+
+void analysisFile(char* secFile, struct Zidian* pzd)
+{
+
+}
